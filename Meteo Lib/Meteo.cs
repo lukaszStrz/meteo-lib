f33a7dc2fd68cb01b5f441c5eb9a90bc57b657cb
Łukasz <lukasz.strz91@gmail.com>
2013-07-30 23:16:34 +0200

    async work

--- Meteo Lib/Meteo.cs
+++ Meteo Lib/Meteo.cs
@@ -1,18 +1,57 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Meteo_Lib
 {
     public class Meteo
     {
-        #region Private functions
-
-        string resolve(Coordinates coord, string urlBeg)
+        /// <summary>
+        /// Downloads an image from given URI.
+        /// </summary>
+        private async Task<MemoryStream> DownloadImgAsync(Uri uri)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(urlBeg);
+            var client = new WebClient();
+            var data = await client.DownloadDataTaskAsync(uri);
+            return new MemoryStream(data);
+        }
+
+        /// <summary>
+        /// Downloads the UM model for given location.
+        /// </summary>
+        /// <param name="coord">Coordinates</param>
+        public async Task<MemoryStream> UM(Coordinates coord)
+        {
+            var uri = await ResolveAsyncUM(coord);
+            var result = await DownloadImgAsync(GetImgUri(uri));
+            return result;
+        }
+
+        /// <summary>
+        /// Downloads the COAMPS model for given location.
+        /// </summary>
+        /// <param name="coord">Coordinates</param>
+        public async Task<MemoryStream> COAMPS(Coordinates coord)
+        {
+            var uri = await ResolveAsyncCOAMPS(coord);
+            var result = await DownloadImgAsync(GetImgUri(uri));
+            return result;
+        }
+
+        #region URI from coordinates
+
+        /// <summary>
+        /// Returns URI with row and col for given location.
+        /// </summary>
+        /// <param name="coord">Coordinates</param>
+        /// <param name="uriBeg">UM/COAMPS search uri</param>
+        private async Task<string> ResolveAsync(Coordinates coord, string uriBeg)
+        {
+            var sb = new StringBuilder();
+            sb.Append(uriBeg);
             sb.Append(@"NALL=");
             sb.Append(coord.NALL);
             sb.Append(@"&EALL=");
@@ -20,22 +59,36 @@ namespace Meteo_Lib
 
             var uri = new Uri(sb.ToString());
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            return response.ResponseUri.ToString();
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            var response = await request.GetResponseAsync();
+            return ((HttpWebResponse)response).ResponseUri.ToString();
         }
 
-        string resolveUM(Coordinates coord)
+        /// <summary>
+        /// Returns UM model URI with row and col for given location.
+        /// </summary>
+        /// <param name="coord">Coordinates</param>
+        private async Task<string> ResolveAsyncUM(Coordinates coord)
         {
-            return resolve(coord, @"http://www.meteo.pl/um/php/mgram_search.php?");
+            var result = await ResolveAsync(coord, @"http://www.meteo.pl/um/php/mgram_search.php?");
+            return result;
         }
 
-        string resolveCOAMPS(Coordinates coord)
+        /// <summary>
+        /// Returns COAMPS model URI with row and col for given location.
+        /// </summary>
+        /// <param name="coord">Coordinates</param>
+        private async Task<string> ResolveAsyncCOAMPS(Coordinates coord)
         {
-            return resolve(coord, @"http://www.meteo.pl/php/mgram_search.php?");
+            var result = await ResolveAsync(coord, @"http://www.meteo.pl/php/mgram_search.php?");
+            return result;
         }
 
-        string getImgUri(string meteoUri)
+        /// <summary>
+        /// Returns model image URI.
+        /// </summary>
+        /// <param name="meteoUri">Model URI</param>
+        private Uri GetImgUri(string meteoUri)
         {
             if (!meteoUri.Contains(@"row=") || !meteoUri.Contains(@"col="))
             {
@@ -44,29 +97,7 @@ namespace Meteo_Lib
 
             string substring = meteoUri.Substring(meteoUri.IndexOf('?'));
 
-            StringBuilder sb = new StringBuilder();
-            sb.Append(@"http://www.meteo.pl/um/metco/mgram_pict.php");
-            sb.Append(substring);
-
-            return sb.ToString();
-        }
-
-        #endregion
-
-        #region Public functions
-
-        public MemoryStream UM(Coordinates coord)
-        {
-            string um = resolveUM(coord);
-            string img = getImgUri(um);
-            return new MemoryStream(new WebClient().DownloadData(img));
-        }
-
-        public MemoryStream COAMPS(Coordinates coord)
-        {
-            string coamps = resolveCOAMPS(coord);
-            string img = getImgUri(coamps);
-            return new MemoryStream(new WebClient().DownloadData(img));
+            return new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php" + substring);
         }
 
         #endregion

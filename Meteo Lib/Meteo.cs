5352892adadd30d503074dee75f5f2dbda64cc51
Łukasz <lukasz.strz91@gmail.com>
2013-07-30 23:26:32 +0200

    main cities

--- Meteo Lib/Meteo.cs
+++ Meteo Lib/Meteo.cs
@@ -101,5 +101,117 @@ namespace Meteo_Lib
         }
 
         #endregion
+
+        #region Main cities UM
+
+        public async Task<MemoryStream> BialystokUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=379&col=285&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> BydgoszczUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=381&col=199&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> GdanskUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=346&col=210&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> GorzowUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=390&col=152&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> KatowiceUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=461&col=215&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> KielceUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=443&col=244&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> KrakowUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=466&col=232&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> LublinUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=432&col=277&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> LodzUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=418&col=223&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> OlsztynUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=363&col=240&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> OpoleUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=449&col=196&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> PoznanUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=400&col=180&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> RzeszowUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=465&col=269&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> SzczecinUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=370&col=142&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> TorunUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=383&col=209&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> WarszawaUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=406&col=250&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> WroclawUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=436&col=181&lang=pl"));
+            return result;
+        }
+
+        public async Task<MemoryStream> ZielonaGoraUM()
+        {
+            var result = await DownloadImgAsync(new Uri(@"http://www.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=412&col=155&lang=pl"));
+            return result;
+        }
+
+        #endregion
     }
 }
